a749489b565bc4634e01b1dee061ef36ae75d70c
YuyaYoshioka <yuya3330930@yahoo.co.jp>
2021-09-03 23:09:18 +0900

    grpcのコードを追加

--- minimart/app/graphql/mutations/create_order.rb
+++ minimart/app/graphql/mutations/create_order.rb
@@ -1,6 +1,11 @@
+require 'minifinancier_services_pb' # 自動生成されたコードのロード
+
 module Mutations
   class CreateOrder < BaseMutation
     # field :client_mutation_id, String, null: false
+    # 自動生成されたコードから gRPC Stub を作成 #=> "payment-42"
+
+
     field :order, Types::OrderType, null: false
 
     argument :pickup_location_id, ID, required: true
@@ -23,6 +28,17 @@ module Mutations
       items.each do |item|
         ProductOrder.create!(order: order, product: Product.find_by(id: item[:product_id]), product_count: item[:quantity])
       end
+
+      service = Minifinancier::PaymentGateway::Stub.new(
+        'localhost:50051',
+        :this_channel_is_insecure,
+        )
+      # gRPC Stub のメソッドを呼ぶと minifinancier の RPC が呼ばれる
+      payment = service.charge(
+        Minifinancier::ChargeRequest.new(user_id: context[:current_user].id, amount: total_amount),
+        )
+      puts payment.id
+
       if order
         { order: order }
       else
